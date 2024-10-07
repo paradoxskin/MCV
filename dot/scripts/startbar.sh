924a7f23009ca9dd1727f85e32be5d8653f8c5a5
paradoxskin <1312269430@qq.com>
2024-10-07 17:08:04 +0800

    fix cstatusbar bug

--- dot/scripts/startbar.sh
+++ dot/scripts/startbar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cstatusbar &
+cstatusbar &> /tmp/cstatusbar.log &
 amixer get Master |awk -F '[]%[]' 'END{printf "V;%d\n", $2}' > /tmp/dwm.fifo
 ~/.scripts/bgl
 echo "T;$(cat ~/TODO)" > /tmp/dwm.fifo
